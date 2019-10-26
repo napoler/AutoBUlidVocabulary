839d8dce40e09357591c857ad501aa27bcdd113b
Terry chan <napoler2008@gmail.com>
2019-10-27 01:42:38 +0800

    添加google vocab文件

--- AutoBUlidVocabulary/vocab.py
+++ AutoBUlidVocabulary/vocab.py
@@ -110,3 +110,13 @@ class Vocab:
 # print("word_dict",word_dict)
 
 #  add_vectorizer(self, text_arr)
+
+
+class GVocab:
+    def __init__(self):
+        pass
+    def load(self):
+        f = open("vocab.txt","r")  
+        lines = f.readlines()#读取全部内容  
+        for line in lines: 
+            print( line  )
---
+++ test.py
@@ -0,0 +1,35 @@
+#encoding=utf-8
+from __future__ import unicode_literals
+import sys
+# sys.path.append("../")
+from AutoBUlidVocabulary import *
+
+# # word_list=["哈士奇","狗子",'niub']
+# vocab=Vocab()
+
+
+# # vocab_list=vocab.get_vectorizer(word_list)
+# # print(vocab_list)
+
+# # vocab_list=vocab.add_vectorizer(word_list)
+# # print(vocab_list)
+
+
+# # c= vocab.text_voc_ids("饿猫饿喜欢而伟长吃饿而我热土什么")
+# # print(c)
+
+# # word_list=['n']
+# text="哦哦喵星人真是太可爱了"
+# word_list=list(text)
+# vocab.add_vectorizer(word_list)
+
+# print(vocab.load())
+
+
+# GVocab().load()
+
+import os
+import sys
+print(os.path.abspath('vocab.txt'))
+os.chdir(sys.path[0])
+# print(os.path.abspath('main.py'))
